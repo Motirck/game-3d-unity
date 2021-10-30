81db8a06460eeca9d4a8104c136740a84b02dab3
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-10-30 19:38:40 -0300

    Added rotation to AddVida icon

--- Assets/Scenes/02/AddVida.cs
+++ Assets/Scenes/02/AddVida.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class AddVida : MonoBehaviour
 {
     public int vida = 10;
+    public float velMovimento;
+    public float eixoX;
+    public float eixoY;
+    public float eixoZ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        velMovimento = 25f;
+        eixoX = 1;
+        eixoY = 0;
+        eixoZ = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(eixoX, eixoY, eixoZ) * velMovimento * Time.deltaTime);
     }
 
     // colidiu com algum colider
